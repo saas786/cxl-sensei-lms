9ee8aed75b0317f8f443e712d69a26a8f7c331e2
leho <leho@conversionready.com>
2020-02-13 00:09:04 +0200

    Disable course assignment teacher notification

--- includes/class-sensei-teacher.php
+++ includes/class-sensei-teacher.php
@@ -363,7 +363,13 @@ class Sensei_Teacher {
 		self::update_course_modules_author( $course_id, $new_author );
 
 		// notify the new teacher
-		$this->teacher_course_assigned_notification( $new_author, $course_id );
+		/**
+		 * How about we don't?
+		 * 
+		 * @author leho
+		 * @since 2020.01.09
+		 */
+		// $this->teacher_course_assigned_notification( $new_author, $course_id );
 
 	} // end save_teacher_meta_box
 
